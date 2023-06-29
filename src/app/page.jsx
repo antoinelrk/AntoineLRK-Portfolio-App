1dc72a0830f52556b0b9b2e1f22fb4d380cf7d58
Antoine LRK <admin@antoinelrk.com>
2023-06-29 13:41:44 +0200

    LRK-29: Testimonials intégration (For desktop only)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,14 @@
+import Testimonials from "@/components/Testimonials/Testimonials";
+
 export default function Home () {
     return (
-        <main>Home</main>
+        <main>
+            {/* Section: Model3D */}
+            {/* Section: Testimonials */}
+            <Testimonials />
+            {/* Section: Formations */}
+            {/* Section: Projects */}
+            {/* Section: Contact */}
+        </main>
     )
 }
