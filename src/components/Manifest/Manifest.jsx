d3b076da4b2bb4a41382834f7688ea254a8b2975
Antoine LRK <admin@antoinelrk.com>
2023-07-01 17:58:38 +0200

    LRK-31: Finish manifest component

--- src/components/Manifest/Manifest.jsx
+++ src/components/Manifest/Manifest.jsx
@@ -5,7 +5,7 @@ export default function Testimonials () {
         <section className={Style.Manifest}>
             <h2 className={Style.sectionTitle}>MANIFEST</h2>
             <p>
-            Passionné de technologie en tout genre depuis plus de 15 ans, j’expérimente des technologies et conçois des outils logiciels simple et efficace pour particulier, association ou entreprise. Ayant une grande appétance pour l’administration système, le “hardware” en général; je souhaites me diriger vers une carrière axée Backend. 
+            Passionné de technologie en tout genre depuis plus de 15 ans, j’expérimente des technologies et conçois des outils logiciels simples et efficaces pour particulier, association ou entreprise. Ayant une grande appétance pour l’administration système, le “hardware” en général; je souhaites me diriger vers une carrière axée Backend. 
 Je favorise la création d’interface pour les webmasters ou bien les content managers.
             </p>
         </section>
