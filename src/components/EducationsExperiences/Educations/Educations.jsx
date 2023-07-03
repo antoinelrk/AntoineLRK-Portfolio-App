36a4bc848ee51ca99dbcc13cad2d6072bccbe796
AntoineLRK <admin@antoinelrk.com>
2023-07-03 10:10:32 +0200

    LRK-33: Continue integration

--- src/components/EducationsExperiences/Educations/Educations.jsx
+++ src/components/EducationsExperiences/Educations/Educations.jsx
@@ -1,7 +1,36 @@
+import Style from './Educations.module.scss'
+import Image from 'next/image'
+import exampleImage from './example.png'
+
 export default function Educations () {
-    return (
-        <>
-            Educations
-        </>
-    )
+
+    const data = [
+        {
+            id: 1,
+            title: "Développeur Intégrateur Web",
+            start_at: new Date(),
+            ended_at: new Date(),
+            company_name: "OpenClassrooms",
+            company_brand: exampleImage
+        },
+        {
+            id: 2,
+            title: "Développeur Intégrateur Web",
+            start_at: new Date(),
+            ended_at: new Date(),
+            company_name: "OpenClassrooms",
+            company_brand: exampleImage
+        }
+    ]
+
+    const content = data.map(element => (
+        <article className={Style.card}>
+            <h3 className={Style.cardTitle}>{element.title}</h3>
+            <p className={Style.educationsDate}>{element.start_at.getTime().toString()} - {element.ended_at.getTime().toString()}<i>-</i><span>OpenClassrooms</span></p>
+            <a className={Style.educationsCertificationLink} target="_blank" href="#">Certification</a>
+            <Image className={Style.educationsCompanyLogo}  src={exampleImage} alt="Logo du centre de formation" />
+        </article>
+    ))
+
+    return (<section className={Style.Educations}>{content}</section>)
 }
