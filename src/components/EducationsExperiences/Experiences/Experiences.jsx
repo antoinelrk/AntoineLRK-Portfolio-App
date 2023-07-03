fa36a79f81cb058c660021e5ada616de944100d1
Antoine LRK <admin@antoinelrk.com>
2023-07-03 13:06:17 +0200

    LRK-33: Implement bloc-tab

--- src/components/EducationsExperiences/Experiences/Experiences.jsx
+++ src/components/EducationsExperiences/Experiences/Experiences.jsx
@@ -1,34 +1,45 @@
 import Style from './Experiences.module.scss'
 import Image from 'next/image'
 import exampleImage from './example.png'
+import { toHumans } from '@/helpers/Date.js'
 
 export default function Experiences () {
+
+    const dateExample = new Date("1994-02-06 12:10:00").toLocaleString('fr-FR', { timeZone: "Europe/Paris" })
+
     const data = [
         {
             id: 1,
             title: "Développeur Intégrateur Web",
-            start_at: new Date(),
-            ended_at: new Date(),
+            start_at: dateExample,
+            ended_at: dateExample,
             company_name: "OpenClassrooms",
-            company_brand: exampleImage
+            company_brand: exampleImage,
+            description: "Passionné de technologie en tout genre depuis plus de 15 ans, j’expérimente des technologies et conçois des outils logiciels simples et efficaces pour particulier, association ou entreprise. Ayant une grande appétance pour l’administration système, le “hardware” en général; je souhaites me diriger vers une carrière axée Backend. Je favorise la création d’interface pour les webmasters ou bien les content managers. Passionné de technologie en tout genre depuis plus de 15 ans, j’expérimente des technologies et conçois des outils logiciels simples et efficaces pour particulier, association ou entreprise. Ayant une grande appétance pour l’administration système, le “hardware” en général; je souhaites me diriger vers une carrière axée Backend. Je favorise la création d’interface pour les webmasters ou bien les content managers."
         },
         {
             id: 2,
             title: "Développeur Intégrateur Web",
-            start_at: new Date(),
-            ended_at: new Date(),
+            start_at: dateExample,
+            ended_at: dateExample,
             company_name: "OpenClassrooms",
-            company_brand: exampleImage
+            company_brand: exampleImage,
+            description: "Passionné de technologie en tout genre depuis plus de 15 ans, j’expérimente des technologies et conçois des outils logiciels simples et efficaces pour particulier, association ou entreprise. Ayant une grande appétance pour l’administration système, le “hardware” en général; je souhaites me diriger vers une carrière axée Backend. Je favorise la création d’interface pour les webmasters ou bien les content managers."
         }
     ]
 
     const content = data.map(element => (
-        <article className={Style.card}>
-            <h3 className={Style.cardTitle}>{element.title}</h3>
-            <p className={Style.educationsDate}>{element.start_at.getTime().toString()} - {element.ended_at.getTime().toString()}<i>-</i><span>OpenClassrooms</span></p>
-            <a href="" className={Style.experiencesCompanyLink}>
-                <Image className={Style.educationsCompanyLogo}  src={exampleImage} alt="Logo du centre de formation" />
-            </a>
+        <article className={Style.card} key={element.id}>
+            <article className={Style.cardContent}>
+                <h3 className={Style.cardTitle}>{element.title}</h3>
+                <p className={Style.metadata}>du {toHumans(element.start_at)} au {toHumans(element.ended_at)}<i>—</i><span>OpenClassrooms</span></p>
+                <p className={Style.description}>{element.description}</p>
+            </article>
+            <aside className={Style.companyBrand}>
+                <a href={element.company_url} className={Style.experiencesCompanyLink}>
+                    <Image className={Style.experiencesCompanyLogo}  src={exampleImage} alt="Logo du centre de formation" />
+                </a>
+            </aside>
         </article>
     ))
 
